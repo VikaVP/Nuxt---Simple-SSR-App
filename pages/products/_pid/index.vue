48ee891ad723c97e998cd11b6d46d6cacee2f905
Vika VP <you@example.com>
2021-04-04 18:39:03 +0700

    nuxt app

---
+++ pages/products/_pid/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <h1>product with ID : {{ $route.params.pid }}</h1>
+  </div>
+</template>
---
+++ pages/products/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/products/1">goes to product 1</nuxt-link>
+    <nuxt-link to="/products/2">goes to product 2</nuxt-link>
+  </div>
+</template>
